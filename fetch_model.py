58da4a9db679c95ee1eec6fa5da2f5b5b56fbf05
Vanessa Timmer <v.timmer@student.tudelft.nl>
2024-05-21 01:10:19 +0200

    added script for fetching trained model

---
+++ fetch_model.py
@@ -0,0 +1,16 @@
+import os
+import ellipsis as el
+
+def fetch_model():
+    """Downloads pretrained model from remote drive."""
+
+    # Ensure the directory exists
+    directory = 'model'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    # Download pretrained model
+    el.path.file.download(pathId='c7894e6f-1a2f-40b2-ab48-01e01e915790', filePath=directory+'\\model.joblib')
+
+if __name__ == "__main__":
+    fetch_model()
