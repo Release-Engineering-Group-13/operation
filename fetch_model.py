01351f082fee6be8f17ba8cc0146fc0d11bcb4a1
vtimmerTUD <160277532+vtimmerTUD@users.noreply.github.com>
2024-05-22 22:54:49 +0200

    Update fetch_model.py
    Co-authored-by: Maartenvb02 <119118538+Maartenvb02@users.noreply.github.com>

--- fetch_model.py
+++ fetch_model.py
@@ -10,7 +10,7 @@ def fetch_model():
         os.makedirs(directory)
     
     # Download pretrained model
-    el.path.file.download(pathId='c7894e6f-1a2f-40b2-ab48-01e01e915790', filePath=directory+'\\model.joblib')
+    el.path.file.download(pathId='c7894e6f-1a2f-40b2-ab48-01e01e915790', filePath=directory+'/model.joblib')
 
 if __name__ == "__main__":
     fetch_model()
